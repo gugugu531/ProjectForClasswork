5818f8089d7073fa6d5fdfbf57c9518cc1398934
U-DESKTOP-K9SGERT\Alan <penaddlight@outlook.com>
2024-12-11 17:12:00 +0800

    第一次版本提交

---
+++ include/Untitled.h
@@ -0,0 +1,34 @@
+#ifndef UNTITLED
+#define UNTITLED
+
+typedef struct
+{
+    int StudentID;
+    char Name[20];
+    int gender;
+    int school;
+    int major;
+    char address[20];
+    int phoneNumber;
+    int qqNumber;
+    char emailAddr[35];
+    Student* next;
+}Student;
+
+
+void ShowTheMenu(void);
+void ClearTheSurface(void);
+void ShowTheData(void);
+void Insert(void);
+void Delete(void);
+void Modify(void);
+void FuzzySearch(void);
+void Sort(void);
+void SaveTheLog(void);
+void SaveTheData(void);
+void ImportTheData(void);
+void ExportTheData(void);
+void SendEmail(void);
+void ReadCmd(void);
+
+#endif
